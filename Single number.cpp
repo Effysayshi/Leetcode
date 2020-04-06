4875ecdc9cf01f2798a66082d284665726e17a1b
saywhattt <37951501+Effysayshi@users.noreply.github.com>
2020-04-06 15:12:48 +0900

    Update Single number.cpp
    add problem description

--- Single number.cpp
+++ Single number.cpp
@@ -1,3 +1,7 @@
+//problem: Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+//example: Input: [2,2,1]
+//         Output: 1
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
